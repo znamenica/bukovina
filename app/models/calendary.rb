be3325e57b321ca0601fcae7d2de7102ed8a8890
majioa <majioa@yandex.ru>
2016-05-18 14:59:20 +0300

    Правка в предыдущий патч.

--- app/models/calendary.rb
+++ app/models/calendary.rb
@@ -7,10 +7,9 @@ class Calendary < ActiveRecord::Base
    accepts_nested_attributes_for :descriptions, reject_if: :all_blank
    accepts_nested_attributes_for :names, reject_if: :all_blank
 
-   # TODO add validation to at least one description and name
    has_alphabeth
 
    validates :slug, :date, presence: true
 
    validates :descriptions, :names, presence: true
-   validates :descriptions, :names, associated: true ; end
+   validates :descriptions, :names, associated: true ;end
---
+++ features/factories/calendaries.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+   factory :calendary do
+      slug { Faker::Internet.slug }
+      date { Faker::Date.forward(23) }
+      author { Faker::Superhero.name }
+      language_code :ру
+      alphabeth_code :ру
+
+      transient do
+         descriptions true
+         names true ;end
+
+      after( :build ) do |calendary, e|
+         if e.descriptions.present?
+            calendary.descriptions = build_list( :description, 1,
+               describable: calendary ) ;end
+         if e.names.present?
+            calendary.names = build_list( :appellation, 1,
+               describable: calendary ) ;end ;end
+
+      trait( :with_invalid_description ) do
+         after( :build ) do |calendary, e|
+            calendary.descriptions = build_list :invalid_description, 1, describable: calendary
+            end ;end ;end ;end
